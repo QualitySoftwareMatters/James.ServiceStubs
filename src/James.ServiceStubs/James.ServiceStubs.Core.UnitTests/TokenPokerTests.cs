443a4f0c9f812c36452bf863224ebfe71660d5b6
Todd Meinershagen <todd@meinershagen.net>
2016-02-27 05:54:15 -0600

    Added TokenPoker test for nested dictionaries.

--- src/James.ServiceStubs/James.ServiceStubs.Core.UnitTests/TokenPokerTests.cs
+++ src/James.ServiceStubs/James.ServiceStubs.Core.UnitTests/TokenPokerTests.cs
@@ -17,11 +17,13 @@ namespace James.ServiceStubs.Core.UnitTests
         [TestCase("http://myurl/{id}/{firstName}", "http://myurl/1/Todd")]
         [TestCase("http://myurl/{Id}/{FirstName}", "http://myurl/1/Todd")]
         [TestCase("http://myurl/{Address.state}/{address.City}/{firstname}", "http://myurl/TX/Frisco/Todd")]
+        [TestCase("http://myurl.com/phones/mobile/{phones.Home}", "http://myurl.com/phones/mobile/972-712-2771")]
         public void given_value_with_tokens_and_existing_data_when_poking_should_return_poked_value(string valueWithTokens, string pokedValue)
         {
             var address = new {City = "Frisco", State = "TX", ZipCode = "75034"};
+            var phones = new Dictionary<string, object> {{ "Home", "972-712-2771"}};
 
-            var data = new Dictionary<string, object> {{"Id", 1}, {"FirstName", "Todd"}, {"LastName", "Meinershagen"}, {"Address", address} };
+            var data = new Dictionary<string, object> {{"Id", 1}, {"FirstName", "Todd"}, {"LastName", "Meinershagen"}, {"Address", address}, {"Phones", phones} };
 
             var poker = new TokenPoker();
             var result = poker.PokeData(valueWithTokens, data);
