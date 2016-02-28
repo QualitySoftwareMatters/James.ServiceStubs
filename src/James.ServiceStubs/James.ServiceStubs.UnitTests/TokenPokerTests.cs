7d23740203ee0ee574d0a69eb6a5c37f96f179ec
Todd Meinershagen <todd@meinershagen.net>
2016-02-27 20:19:45 -0600

    Renamed the unit tests project to match the James.ServiceStubs main core project.

---
+++ src/James.ServiceStubs/James.ServiceStubs.UnitTests/TokenPokerTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+using FluentAssertions;
+
+using NUnit.Framework;
+
+namespace James.ServiceStubs.UnitTests
+{
+    [TestFixture]
+    public class TokenPokerTests
+    {
+        [TestCase("http://myurl/{id}/{firstName}", "http://myurl/1/Todd")]
+        [TestCase("http://myurl/{Id}/{FirstName}", "http://myurl/1/Todd")]
+        [TestCase("http://myurl/{Address.state}/{address.City}/{firstname}", "http://myurl/TX/Frisco/Todd")]
+        [TestCase("http://myurl.com/phones/mobile/{phones.Home}", "http://myurl.com/phones/mobile/972-712-2771")]
+        public void given_value_with_tokens_and_existing_data_when_poking_should_return_poked_value(string valueWithTokens, string pokedValue)
+        {
+            var address = new {City = "Frisco", State = "TX", ZipCode = "75034"};
+            var phones = new Dictionary<string, object> {{ "Home", "972-712-2771"}};
+            var data = new Dictionary<string, object> {{"Id", 1}, {"FirstName", "Todd"}, {"LastName", "Meinershagen"}, {"Address", address}, {"Phones", phones} };
+
+            var poker = new TokenPoker();
+            var result = poker.PokeData(valueWithTokens, data);
+
+            result.Should().Be(pokedValue);
+        }
+    }
+}
