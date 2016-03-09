912494bbc6f73024de447942ab86bc357e6f4cd0
Todd Meinershagen <todd@meinershagen.net>
2016-03-09 06:55:41 -0600

    Added FileProvider for testing purposes and renamed the TemplateProvider to FileTemplateProvider to distinguish it.

---
+++ src/James.ServiceStubs/James.ServiceStubs/FileProvider.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+
+namespace James.ServiceStubs
+{
+    public class FileProvider : IFileProvider
+    {
+        public bool Exists(string path)
+        {
+            return File.Exists(path);
+        }
+
+        public string ReadAllText(string path)
+        {
+            return File.ReadAllText(path);
+        }
+    }
+}
---
+++ src/James.ServiceStubs/James.ServiceStubs/FileTemplateProvider.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace James.ServiceStubs
+{
+    public class FileTemplateProvider : ITemplateProvider
+    {
+        private readonly ILogger _logger;
+        private readonly IFileProvider _fileProvider;
+        private readonly string _filePath;
+        public const string TemplateDoesNotExistFormat = "The path configured for the template({0} does not exist.  Empty content will be returned.";
+
+        public FileTemplateProvider(ILogger logger, IFileProvider fileProvider, string filePath)
+        {
+            _filePath = filePath;
+            _logger = logger;
+            _fileProvider = fileProvider;
+        }
+
+        public FileTemplateProvider(ILogger logger, IFileProvider fileProvider)
+            : this(logger, fileProvider, Environment.CurrentDirectory)
+        {
+        }
+
+        public string GetContentsFor(string templateKey, IDictionary<string, object> parameters)
+        {
+            var templatePath = Path.Combine(_filePath, templateKey);
+
+            if (_fileProvider.Exists(templatePath))
+            {
+                return _fileProvider.ReadAllText(templatePath);
+            }
+
+            _logger.Error(TemplateDoesNotExistFormat, templatePath);
+            return string.Empty;
+        }
+    }
+}
