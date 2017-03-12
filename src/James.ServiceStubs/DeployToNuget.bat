fd607a0df08033806968d020739bdb6e93e934ac
Todd Meinershagen <todd@meinershagen.net>
2017-03-12 03:51:45 -0500

    Updating the deploy to nuget script to have a Source attribute when pushing to nuget.  Also, updated the latest version of the package for reference on the github site.

--- src/James.ServiceStubs/DeployToNuget.bat
+++ src/James.ServiceStubs/DeployToNuget.bat
@@ -29,5 +29,5 @@ nuget.exe pack -Build -OutputDirectory build %package% -Prop Configuration=Relea
 REM Push to Nuget 
 REM =============
 cd build
-nuget.exe push James.ServiceStubs.*.nupkg
+nuget.exe push James.ServiceStubs.*.nupkg -Source https://www.nuget.org/api/v2/package
 cd ..
